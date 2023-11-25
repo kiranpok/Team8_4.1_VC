b30b9bfa8be85dc7df517fa5ced0347796d404d5
dungdpham <dung.pham3@metropolia.fi>
2023-11-25 21:44:45 +0200

    push own branch

--- Team8/src/PalindromeChecker.java
+++ Team8/src/PalindromeChecker.java
@@ -1,11 +1,14 @@
+import java.util.Scanner; // Include import for resource
+
 public class PalindromeChecker {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please input your string: "); // Narration for user-friendliness
         String input = scanner.nextLine();
 
         String transformed = transformInput(input);
+        System.out.println("\nYour input transformed to \"" + transformed + "\".");
 
         boolean isPalindrome = checkPalindrome(transformed);
         if (isPalindrome) {
@@ -24,18 +27,23 @@ public class PalindromeChecker {
 
     public static boolean checkPalindrome(String input) {
         String reversed = reverseString(input);
-        if (input.equals(reversed)) {
+
+        /*if (input.equals(reversed)) {
             return true;
         } else {
             return false;
-        }
+        }*/
+
+        return (input.equals(reversed)); // Simplified original if-else code
     }
 
     public static String reverseString(String input) {
         StringBuilder reversed = new StringBuilder();
+
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed.append(input.charAt(i));
         }
+        
         return reversed.toString();
     }
 }
