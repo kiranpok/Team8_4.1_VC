5f72fef9a5390687262896cbe19361ee2817dce8
Artur Gol <artur.golavskiy@metropolia.fi>
2023-11-26 02:30:30 +0200

    fix: merge error

--- Team8/src/PalindromeChecker.java
+++ Team8/src/PalindromeChecker.java
@@ -8,9 +8,7 @@ public class PalindromeChecker {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-      
         System.out.print("Please input your string: "); // Narration for user-friendliness
-
         String input = scanner.nextLine();
 
         // Transform the input
@@ -40,34 +38,18 @@ public class PalindromeChecker {
      //Checks if a given string is a palindrome.
 
     public static boolean checkPalindrome(String input) {
-
         String reversed = reverseString(input);
-        /*if (input.equals(reversed)) {
-
-            return true;
-        } else {
-            return false;
-        }*/
-
-        return (input.equals(reversed)); // Simplified original if-else code
-    }
-
-    public static String transformInput(String in) {
-        in = in.toLowerCase();
-        in = in.replace(",", "");
-        in = in.replace("!", "");
-        return in;
+        return (input.equals(reversed));
     }
 
     //Reverses a given string.
 
     public static String reverseString(String input) {
-
         StringBuilder reversed = new StringBuilder();
-
         for (int i = input.length() - 1; i >= 0; i--) {
-            stringBuilder.append(input.charAt(i));
+            reversed.append(input.charAt(i));
         }
         return reversed.toString();
     }
+
 }
